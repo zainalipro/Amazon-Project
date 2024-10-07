d9c9ef3eade11ccaac68e2f122c22a72febcdea1
Zain Ali <zainali.mrpro@gmail.com>
2024-10-07 15:37:51 +0500

    move displayItems() into products.js file and move addToCard() into cards.js file

--- data/cards.js
+++ data/cards.js
@@ -1,1 +1,21 @@
-export const cards = [];
+export const card = [];
+
+// add to card function
+export function addToCard(productId) {
+    let matchingItem;
+    // checking  if the product is already in the cart or not.
+    card.forEach((cardItem) => {
+        if (productId === cardItem.productId) {
+            matchingItem = cardItem;
+        }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity++;
+    } else {
+        card.push({
+            productId,
+            quantity: 1
+        });
+    }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,89 +1,25 @@
-import { cards as card } from '../data/cards.js';
-import { products } from '../data/products.js';
+import { displayItems } from '../data/products.js';
+import { addToCard } from '../data/cards.js';
 
-const displayProducts = document.getElementById('js-products-grid');
-
-let productsHTML = '';
-// load the  products from the file products.js
-products.forEach((product) => {
-  productsHTML += `
-          <div class="product-container">
-        <div class="product-image-container">
-          <img class="product-image" src="${product.image}">
-        </div>
-
-        <div class="product-name limit-text-to-2-lines">
-          ${product.name}
-        </div>
-
-        <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-${(product.rating['stars']) * 10}.png">
-          <div class="product-rating-count link-primary">
-           ${product.rating['count']}
-          </div>
-        </div>
-
-        <div class="product-price">
-          $${(product.priceCents / 100).toFixed(2)}
-        </div>
-
-        <div class="product-quantity-container">
-          <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
-
-        <div class="product-spacer"></div>
-
-        <div class="added-to-cart">
-          <img src="images/icons/checkmark.png">
-          Added
-        </div>
-
-        <button class="add-to-cart-button button-primary js-add-to-card-button" data-product-id= "${product.id}">
-          Add to Cart
-        </button>
-      </div>
-    `;
-});
 // display the products onto the page.
-displayProducts.innerHTML += productsHTML;
+window.addEventListener('load', () => {
+  document.getElementById('js-products-grid')
+    .innerHTML = displayItems();
+});
+
+function updateCardQuantity() {
+  let cardQuantity = 0;
+  card.forEach((cardItem) => {
+    cardQuantity += cardItem.quantity;
+  });
+  document.querySelector('.js-card-quantity')
+    .textContent = cardQuantity;
+}
 
 document.querySelectorAll('.js-add-to-card-button').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-
-    // loop to check if the product is already available inside the list?
-    let matchingItem;
-    card.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity++;
-    } else {
-      card.push({
-        productId,
-        quantity: 1
-      });
-    }
-    let cardQuantity = 0;
-    card.forEach((item) => {
-      cardQuantity += item.quantity;
-    });
-    document.querySelector('.js-card-quantity')
-      .textContent = cardQuantity;
+    addToCard(productId);
+    updateCardQuantity();
   });
-
 });
