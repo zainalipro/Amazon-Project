72759b5cf23a11fb036068459a918ddd761304d4
Zain Ali <zainali.mrpro@gmail.com>
2024-11-21 17:33:13 +0500

    create the class version of card

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import orderSummary from "./checkout/orderSummary.js";
 import paymentSummary from "./checkout/paymentSummary.js";
 import renderCheckoutHeader from "./checkout/checkoutHeader.js";
-import '../data/cart-oop.js';
+// import '../data/cart-oop.js';
+import '../data/card-class.js';
 
 renderCheckoutHeader();
 orderSummary();
